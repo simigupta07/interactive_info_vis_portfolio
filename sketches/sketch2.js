15f27bf6e7fbee9cc4d37f67c5d37e28fcd2fd58
simigupta07 <sgupta18@uw.edu>
2026-02-06 13:12:50 -0800

    Added hour, minute, second hand

--- sketches/sketch2.js
+++ sketches/sketch2.js
@@ -5,10 +5,8 @@ registerSketch('sk2', function (p) {
   };
   p.draw = function () {
     p.background(220);
-    p.fill(100, 150, 240);
-    p.textSize(32);
-    p.textAlign(p.CENTER, p.CENTER);
-    p.text('HWK #4. A', p.width / 2, p.height / 2);
-  };
-  p.windowResized = function () { p.resizeCanvas(p.windowWidth, p.windowHeight); };
+    const h = p.hour();
+    const m = p.minute();
+    const s = p.second();
+  }
 });
