0dea8b61bfac863422775fb0b983f770bc6f3cbb
simigupta07 <sgupta18@uw.edu>
2026-02-06 17:27:22 -0800

    Add analog clock body

--- sketches/sketch2.js
+++ sketches/sketch2.js
@@ -1,22 +1,25 @@
-// Instance-mode sketch for tab 2
 registerSketch('sk2', function (p) {
   p.setup = function () {
     p.createCanvas(p.windowWidth, p.windowHeight);
-  };
-  p.draw = function () {
-    p.background(220);
-    p.fill(100, 150, 240);
-    p.textSize(32);
-    p.textAlign(p.CENTER, p.CENTER);
-    p.text('HWK #4. A', p.width / 2, p.height / 2);
-  };
-  p.windowResized = function () { p.resizeCanvas(p.windowWidth, p.windowHeight); };
-    const h = p.hour();
-    const m = p.minute();
-    const s = p.second();
-
-    p.fill(50);
-    p.stroke(200);
-    p.strokeWeight(4);
-    p.rect(125, 175, 250, 150, 20);
-  });
+    angleMode(DEGREES); 
+    textAlign(CENTER, CENTER);
+  }
+  function draw() {
+    background(30);
+  
+    let h = hour();
+    let m = minute();
+    let s = second();
+  
+    //Board behind clock
+    fill(50);
+    stroke(200);
+    strokeWeight(4);
+    rect(125, 175, 250, 150, 20);
+  
+    // Legs
+    stroke(200);
+    line(165, 325, 145, 360);
+    line(335, 325, 355, 360);
+  }
+})
