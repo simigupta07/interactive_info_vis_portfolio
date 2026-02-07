cb5710caffc283c15618bd6a60fcd5292de5e877
simigupta07 <sgupta18@uw.edu>
2026-02-06 20:45:08 -0800

    adding the legs to the clock body

--- sketches/sketch4.js
+++ sketches/sketch4.js
@@ -1,8 +1,9 @@
-function setup() {
+registerSketch("sk4", function (p) {
+p.setup = function setup() {
   createCanvas(500, 500);
-  textAlign(CENTER, CENTER);
-  rectMode(CENTER);
-}
+  textAlign(p.CENTER, p.CENTER);
+  rectMode(p.CENTER);
+};
   
   p.draw = function() {
   p.background(25);
@@ -16,4 +17,17 @@ function setup() {
   p.stroke(180);
   p.strokeWeight(5);
   p.rect(p.width/2, p.height/2, 320, 180, 25);
+
+   //the bar that shows the grill
+   noStroke();
+   fill(60);
+   rect(width/2, height/2 - 70, 260, 20, 10);
+ 
+   //legs
+   stroke(180);
+   strokeWeight(4);
+   line(width/2 - 120, height/2 + 90, width/2 - 140, height/2 + 115);
+   line(width/2 + 120, height/2 + 90, width/2 + 140, height/2 + 115);
+
   }
+});
