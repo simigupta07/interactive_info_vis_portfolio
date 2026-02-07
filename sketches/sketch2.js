568fd0ee390fcf80d3524204f7e1005cf115ea53
simigupta07 <sgupta18@uw.edu>
2026-02-06 17:56:49 -0800

    clock hands

--- sketches/sketch2.js
+++ sketches/sketch2.js
@@ -9,23 +9,11 @@ registerSketch("sk2", function (p) {
 
   p.draw = function () {
     p.background(30);
-
-    // Title text
-    p.fill(100, 150, 240);
-    p.textSize(32);
-    p.text("HWK #4. A", p.width / 2, 50);
-
+    
     // Time (instance mode uses p.hour(), p.minute(), p.second())
     let h = p.hour();
     let m = p.minute();
     let s = p.second();
-
-    // Example: show digital time (optional but useful while debugging)
-    p.fill(220);
-    p.textSize(20);
-    p.text(`${h.toString().padStart(2, "0")}:${m.toString().padStart(2, "0")}:${s
-      .toString()
-      .padStart(2, "0")}`, p.width / 2, 85);
 
     // Board behind clock
     p.fill(50);
@@ -37,5 +25,25 @@ registerSketch("sk2", function (p) {
     p.stroke(200);
     p.line(180, 375, 145, 420);
     p.line(520, 375, 555, 420);
-  };
+
+    // Analog clock face
+  push();
+  translate(width / 2, 250); // center of clock
+
+  stroke(200);
+  strokeWeight(4);
+  fill(50);
+  ellipse(0, 0, 200, 200); // clock circle
+
+  //Hour marks
+  strokeWeight(2);
+  for (let i = 0; i < 12; i++) {
+    let angle = i * 30;
+    let x1 = cos(angle - 90) * 80;
+    let y1 = sin(angle - 90) * 80;
+    let x2 = cos(angle - 90) * 90;
+    let y2 = sin(angle - 90) * 90;
+    line(x1, y1, x2, y2);
+  }
+}
 });
