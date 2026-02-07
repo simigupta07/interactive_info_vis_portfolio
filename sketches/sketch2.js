ff2443446494aa51f14628aa76a80ee7cf66898f
simigupta07 <sgupta18@uw.edu>
2026-02-06 17:38:41 -0800

    update clock div num

--- sketches/sketch2.js
+++ sketches/sketch2.js
@@ -3,9 +3,13 @@ registerSketch('sk2', function (p) {
     p.createCanvas(p.windowWidth, p.windowHeight);
     angleMode(DEGREES); 
     textAlign(CENTER, CENTER);
-  }
+  };
   function draw() {
     background(30);
+    p.fill(100, 150, 240);
+    p.textSize(32);
+    p.textAlign(p.CENTER, p.CENTER);
+    p.text('HWK #4. A', p.width / 2, p.height / 2);
   
     let h = hour();
     let m = minute();
