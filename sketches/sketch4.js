8f599d7e49644a1de12fc8c8325c31d7484912da
simigupta07 <sgupta18@uw.edu>
2026-02-06 20:40:36 -0800

    adding the clock body

--- sketches/sketch4.js
+++ sketches/sketch4.js
@@ -1,14 +1,19 @@
-// Instance-mode sketch for tab 4
-registerSketch('sk4', function (p) {
-  p.setup = function () {
-    p.createCanvas(p.windowWidth, p.windowHeight);
-  };
-  p.draw = function () {
-    p.background(200, 240, 200);
-    p.fill(30, 120, 40);
-    p.textSize(32);
-    p.textAlign(p.CENTER, p.CENTER);
-    p.text('HWK #4. C', p.width / 2, p.height / 2);
-  };
-  p.windowResized = function () { p.resizeCanvas(p.windowWidth, p.windowHeight); };
-});
+function setup() {
+  createCanvas(500, 500);
+  textAlign(CENTER, CENTER);
+  rectMode(CENTER);
+}
+  
+  p.draw = function() {
+  p.background(25);
+  
+  let h = p.hour();
+  let m = p.minute();
+  let s = p.second();
+  
+  // clock body
+  p.fill(40);
+  p.stroke(180);
+  p.strokeWeight(5);
+  p.rect(p.width/2, p.height/2, 320, 180, 25);
+  }
