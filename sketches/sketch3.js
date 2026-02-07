184301677e9a28ab6bbaad7a5668be281ebd92b6
simigupta07 <sgupta18@uw.edu>
2026-02-06 19:54:16 -0800

    making sure it shows up

--- sketches/sketch3.js
+++ sketches/sketch3.js
@@ -7,42 +7,44 @@ registerSketch("sk3", function (p) {
 
   p.draw = function () {
     p.background(30);
-    p.translate(width / 2, height / 2);
+    p.translate(p.width / 2, p.height / 2);
 
-  //set up time based on hour, minute, second
-  let h = p.hour() % 12;
-  let m = p.minute();
-  let s = p.second();
+    // set up time
+    let h = p.hour() % 12;
+    let m = p.minute();
+    let s = p.second();
 
-  let secProgress = s / 60;
-  let minProgress = (m + s / 60) / 60;
-  let hourProgress = (h + m / 60) / 12;
+    let secProgress = s / 60;
+    let minProgress = (m + s / 60) / 60;
+    let hourProgress = (h + m / 60) / 12;
 
-  // Draw rings to simulate apple watch
-  drawRing(240, secProgress, p.color(255, 80, 80));   // seconds
-  drawRing(185, minProgress, p.color(80, 255, 120));  // minutes
-  drawRing(130, hourProgress, p.color(80, 180, 255)); // hours
+    // rings
+    drawRing(240, secProgress, p.color(255, 80, 80));
+    drawRing(185, minProgress, p.color(80, 255, 120));
+    drawRing(130, hourProgress, p.color(80, 180, 255));
 
-   // Digital time in center
-   p.noStroke();
-   p.fill(255);
-   p.textAlign(CENTER, CENTER);
-   p.textSize(28);
-   p.text(
-     p.nf(p.hour(), 2) + ":" + p.nf(m, 2) + ":" + p.nf(s, 2),
-     0,
-     0
-   );
- }
- //function to draw the ring
- function drawRing(size, progress, col) {
-  p.stroke(50);
-  p.strokeWeight(22);
-  p.noFill();
-  p.ellipse(0, 0, size, size);
+    // digital time
+    p.noStroke();
+    p.fill(255);
+    p.textAlign(p.CENTER, p.CENTER); // <-- fixed
+    p.textSize(28);
+    p.text(
+      p.nf(p.hour(), 2) + ":" + p.nf(m, 2) + ":" + p.nf(s, 2),
+      0,
+      0
+    );
+  };
 
-  p.stroke(col);
-  let angle = map(progress, 0, 1, 0, 360);
-  p.arc(0, 0, size, size, -90, -90 + angle);
-}
-})
+  //function to draw the ring
+  function drawRing(size, progress, col) {
+    p.stroke(50);
+    p.strokeWeight(22);
+    p.noFill();
+    p.ellipse(0, 0, size, size);
+
+    p.stroke(col);
+    let angle = p.map(progress, 0, 1, 0, 360);
+    p.arc(0, 0, size, size, -90, -90 + angle);
+  }
+});
+
